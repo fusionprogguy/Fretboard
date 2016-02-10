89b45acc856099e0f5a345f286f6519e3e75ebf7
Steven Muschalik <steven.muschalik@gmail.com>
2016-02-10 12:27:16 +1100

    New Exercises!

--- Exercises.py
+++ Exercises.py
@@ -1,6 +1,127 @@
-#Simple patterns for warmup exercises
+# Scale Sequences & Melodic Patterns
 
 pattern = []
+scalesequence = []
+
+
+scalesequence.append({"Exercise":'7 Note Scale', "Note Grouping":[1], "Sequence":[1,2,3,4,5,6,7]})
+
+scalesequence.append({"Exercise":'3 Note Chord', "Note Grouping":[3], "Sequence":[1,2,3]})
+
+scalesequence.append({"Exercise":'4 Note Chord', "Note Grouping":[4], "Sequence":[1,2,3,4]})
+
+# Important Scale Sequences & Melodic Patterns
+# http://www.pbguitarstudio.com/pdf_files/GUITAR_PDF/Important_Scale_Sequences.pdf
+
+# http://www.guitarplayer.com/lessons/1014/scale-sequences-15-hot-patterns-that-will-fire-up-your-solos---tab/53259
+
+# Diatonic Seconds: 1-2, 2-3, 3-4, 4-5, 5-6, 6-7, 7-1
+scalesequence.append({"Exercise":'Diatonic Seconds', "Note Grouping":[2], "Sequence":[1,2,2,3,3,4,4,5,5,6,6,7,7,1]})
+
+# Diatonic Thirds: 1-3, 2-4, 3-5, 4-6, 5-7, 6-1, 7-2, 1-3
+scalesequence.append({"Exercise":'Diatonic Thirds', "Note Grouping":[2], "Sequence":[1,3,2,4,3,5,4,6,5,7,6,1,7,2,1,3]})
+
+# Diatonic Fourths: 1-4, 2-5, 3-6, 4-7, 5-1, 6-2, 7-3, 1-4
+scalesequence.append({"Exercise":'Diatonic Fourths No. 1', "Note Grouping":[2], "Sequence":[1,4,2,5,3,6,4,7,5,1,6,2,7,3,1,4]})
+
+# Diatonic Fourths: 1-5, 7-4, 6-3, 5-2, 4-1, 3-7, 2-6
+scalesequence.append({"Exercise":'Diatonic Fourths No. 2', "Note Grouping":[2], "Sequence":[1,5,7,4,6,3,5,2,4,1,3,7,2,6]})
+
+# Diatonic Fifths: 1-5, 2-6, 3-7, 4-1, 5-2, 6-3, 7-4, 1-5
+scalesequence.append({"Exercise":'Diatonic Fifths No. 1', "Note Grouping":[2], "Sequence":[1,5,2,6,3,7,4,1,5,2,6,3,7,4,1,5]})
+
+# Diatonic Fifths: 1-4-3-7, 6-2-1-5, 4-7-6-3, 2-5-4-1
+scalesequence.append({"Exercise":'Diatonic Fifths No. 2', "Note Grouping":[4], "Sequence":[1,4,3,7,6,2,1,5,4,7,6,3,2,5,4,1]})
+
+# Diatonic Sixths: 1-6, 2-7, 3-1, 4-2, 5-3, 6-4, 7-5, 1-6
+scalesequence.append({"Exercise":'Diatonic Sixths', "Note Grouping":[2], "Sequence":[1,6,2,7,3,1,4,2,5,3,6,4,7,5,1,6]})
+
+# Diatonic Sevenths: 1-7, 2-(8), 3-2, 4-3, 5-4, 6-5, 7-6, 1-7
+scalesequence.append({"Exercise":'Diatonic Sevenths', "Note Grouping":[2], "Sequence":[1,7,2,8,3,2,4,3,5,4,6,5,7,6,1,7]})
+
+# Diatonic Octaves: 1-(8), 2-9, 3-10, 4-11, 5-12, 6-13, 7-14, 8-15
+scalesequence.append({"Exercise":'Diatonic Octaves', "Note Grouping":[2], "Sequence":[1,8,2,9,3,10,4,11,5,12,6,13,7,14,8,15]})
+
+# Diatonic Thirds Sawtooth: 1-3-4-2, 3-5-6-4, 5-7-1-6, 7-2-3-1
+scalesequence.append({"Exercise":'Diatonic Thirds Sawtooth', "Note Grouping":[4], "Sequence":[1,3,4,2,3,5,6,4,5,7,1,6,7,2,3,1]})
+
+
+# Common Scale Sequences:
+
+# Four Note Sequence No. 1: 1-2-3-4, 2-3-4-5, 3-4-5-6, 4-5-6-7, 5-6-7-1, 6-7-1-2, 7-1-2-3, 1-2-3-4
+scalesequence.append({"Exercise":'Four Note Sequence No. 1', "Note Grouping":[4], "Sequence":[1,2,3,4,2,3,4,5,3,4,5,6,4,5,6,7,5,6,7,1,6,7,1,2,7,1,2,3,1,2,3,4]})
+
+# Three Note Sequence No. 1: 1-2-3, 2-3-4, 3-4-5, 4-5-6, 5-6-7, 6-7-1, 7-1-2, 1-2-3
+scalesequence.append({"Exercise":'Three Note Sequence No. 1', "Note Grouping":[3], "Sequence":[1,2,3,2,3,4,3,4,5,4,5,6,5,6,7,6,7,1,7,1,2,1,2,3]})
+
+# Triplet Pattern No. 1: 1-2-1, 2-3-2, 3-4-3, 4-5-4, 5-6-5, 6-7-6, 7-1-7
+scalesequence.append({"Exercise":'Triplet Pattern No. 1', "Note Grouping":[3], "Sequence":[1,2,1,2,3,2,3,4,3,4,5,4,5,6,5,6,7,6,7,1,7]})
+
+# Triplet Pattern No. 2: 1-7-6, 7-6-5, 6-5-4, 5-4-3, 4-3-2, 3-2-1
+scalesequence.append({"Exercise":'Triplet Pattern No. 1', "Note Grouping":[3], "Sequence":[1,7,6,7,6,5,6,5,4,5,4,3,4,3,2,3,2,1]})
+
+# Triad Arpeggio Sequence No. 1: 1-3-5, 2-4-6, 3-5-7, 4-6-1, 5-7-2, 6-1-3, 7-2-4
+scalesequence.append({"Exercise":'Triad Arpeggio Sequence No. 1', "Note Grouping":[3], "Sequence":[1,3,5,2,4,6,3,5,7,4,6,1,5,7,2,6,1,3,7,2,4]})
+
+# Triad Arpeggio Sequence No. 2: 1-3-5, 6-4-2, 3-5-7, 1-6-4, 5-7-2, 3-1-6, 7-2-4
+scalesequence.append({"Exercise":'Triad Arpeggio Sequence No. 2', "Note Grouping":[3], "Sequence":[1,3,5,6,4,2,3,5,7,1,6,4,5,7,2,3,1,6,7,2,4]})
+
+# Four Note Arpeggio Sequence No. 1: 1-3-5-7, 2-4-6-1, 3-5-7-2, 4-6-1-3, 5-7-2-4, 6-1-3-5, 7-2-4-6, 1-3-5-7
+scalesequence.append({"Exercise":'Four Note Arpeggio Sequence No. 1', "Note Grouping":[4], "Sequence":[1,3,5,7,2,4,6,1,3,5,7,2,4,6,1,3,5,7,2,4,6,1,3,5,7,2,4,6,1,3,5,7]})
+
+# Four Note Arpeggio Sequence No. 2 (bebop pattern): 1-3-5-7, 1-6-4-2, 3-5-7-2, 3-1-6-4, 5-7-2-4, 5-3-1-6, 7-2-4-6, 7-5-3-1
+scalesequence.append({"Exercise":'Four Note Arpeggio Sequence No. 2 (bebop pattern)', "Note Grouping":[4], "Sequence":[1,3,5,7,1,6,4,2,3,5,7,2,3,1,6,4,5,7,2,4,5,3,1,6,7,2,4,6,7,5,3,1]})
+
+
+# Use these patterns for any 5-note scale eg Major and Minor Pentatonic with (Scale Degrees [1,2,3,4,5])
+
+# Scale Sequences using the Major Pentatonic Scale (Scale degrees: 1, 2, 3, 5, 6 of the Major scale) / # Scale Sequences using the Minor Pentatonic Scale  (Scale degrees: 1, b3, 4, 5, b7 of the major scale)
+
+# Major Pentatonic Pattern No. 1: 1-2, 2-3, 3-5, 5-6, 6-1 / # Minor Pentatonic Pattern No. 1: 1-b3, b3-4, 4-5, 5-b7, b7-1
+scalesequence.append({"Exercise":'Pentatonic Pattern No. 1', "Note Grouping":[2], "Sequence":[1,2,2,3,3,4,4,5,5,1]})
+
+# Major Pentatonic Pattern No. 2: 1-3, 2-5, 3-6, 5-1, 6-2, 1-3 / # Minor Pentatonic Pattern No. 2: 1-4, b3-5, 4-b7, 5-1, b7-b3, 1-4
+scalesequence.append({"Exercise":'Pentatonic Pattern No. 2', "Note Grouping":[2], "Sequence":[1,3,2,4,3,5,4,1,5,2,1,3]})
+
+# Major Pentatonic Pattern No. 3: 1-5, 2-6, 3-1, 5-2, 6-3, 1-5 / # Minor Pentatonic Pattern No. 3: 1-5, b3-b7, 4-1, 5-b3, b7-4, 1-5
+scalesequence.append({"Exercise":'Pentatonic Pattern No. 3', "Note Grouping":[2], "Sequence":[1,4,2,5,3,1,4,2,5,3,1,4]})
+
+# Major Pentatonic Pattern No. 4: 1-6, 2-1, 3-2, 5-3, 6-5, 1-6 / # Minor Pentatonic Pattern No. 3: 1-b7, b3-1, 4-b3, 5-4, b7-5, 1-b7
+scalesequence.append({"Exercise":'Pentatonic Pattern No. 4', "Note Grouping":[2], "Sequence":[1,5,2,1,3,2,4,3,5,4,1,5]})
+
+# Major Pentatonic Pattern No. 5: 1-3-6, 2-5-1, 3-6-2, 5-1-3, 6-2-5, 1-3-6 / # Minor Pentatonic Pattern No. 4: 1-4-b7, b3-5-1, 4-b7-b3, 5-1-4, b7-b3-5, 1-4-b7
+scalesequence.append({"Exercise":'Pentatonic Pattern No. 4', "Note Grouping":[3], "Sequence":[1,3,5,2,4,1,3,5,2,4,1,3,5,2,4,1,3,5]})
+
+# Major Pentatonic Pattern No. 6: 1-2-3-5, 3-5-6-1, 6-1-2-3, 2-3-5-6, 5-6-1-2 / # Minor Pentatonic Pattern No. 6: 1-b3-4-5, 4-5-b7-1, b7-1-b3-4, b3-4-5-b7, 5-b7-1-b3
+scalesequence.append({"Exercise":'Pentatonic Pattern No. 5', "Note Grouping":[4], "Sequence":[1,2,3,4,3,4,5,1,5,1,2,3,2,3,4,5,4,5,1,2]})
+
+# Major Pentatonic Pattern No. 7: 1-2-3-5-6-1, 3-5-6-1-2-3, 6-1-2-3-5-6, 2-3-5-6-1-2 / # Minor Pentatonic Pattern No. 7: 1-b3-4-5-b7-1, 4-5-b7-1-b3-4, b7-1-b3-4-5-b7, b3-4-5-b7-1-b3
+scalesequence.append({"Exercise":'Pentatonic Pattern No. 7', "Note Grouping":[6], "Sequence":[1,2,3,4,5,1,3,4,5,1,2,3,5,1,2,3,4,5,2,3,4,5,1,2]})
+
+# Major Pentatonic Pattern No. 8: 1-5-6-3, 3-1-2-6, 6-3-5-2 / # Minor Pentatonic Pattern No. 8: 1-5-b7-4, 4-1-b3-b7, b7-4-5-b3
+scalesequence.append({"Exercise":'Pentatonic Pattern No. 8', "Note Grouping":[4], "Sequence":[1,4,5,3,3,1,2,5,5,3,4,2]})
+
+
+
+# Major Pentatonic Four Note Sequence No. 1: 1-2-3-5, 2-3-5-6, 3-5-6-1, 5-6-1-2, 6-1-2-3 / # Minor Pentatonic Four Note Sequence No. 1: 1-b3-4-5, b3-4-5-b7, 4-5-b7-1, 5-b7-1-b3, b7-1-b3-4
+scalesequence.append({"Exercise":'Pentatonic Four Note Sequence No. 1', "Note Grouping":[4], "Sequence":[1,2,3,4,2,3,4,5,3,4,5,1,4,5,1,2,5,1,2,3]})
+
+# Major Pentatonic Three Note Sequence No. 1: 1-2-3, 2-3-5, 3-5-6, 5-6-1, 6-1-2, 1-2-3 / # Minor Pentatonic Three Note Sequence No. 1: 1-b3-4, b3-4-5, 4-5-b7, 5-b7-1, b7-1-b3, 1-b3-4
+scalesequence.append({"Exercise":'Pentatonic Three Note Sequence No. 1', "Note Grouping":[3], "Sequence":[1,2,3,2,3,4,3,4,5,4,5,1,5,1,2,1,2,3]})
+
+# Major Pentatonic Triplet Pattern No. 1: 1-2-1, 2-3-2, 3-5-3, 5-6-5, 6-1-6, 1-2-1 / # Minor Pentatonic Triplet Pattern No. 1: 1-b3-1, b3-4-b3, 4-5-4, 5-b7-5, b7-1-b7, 1-b3-1
+scalesequence.append({"Exercise":'Pentatonic Triplet Pattern No. 1', "Note Grouping":[3], "Sequence":[1,2,1,2,3,2,3,4,3,4,5,4,5,1,5,1,2,1]})
+
+# Major Pentatonic Three Note Arpeggio Sequence No. 1: 1-3-5, 2-5-6, 3-6-1, 5-1-2, 6-2-3, 1-3-5 / # Minor Pentatonic Three Note Arpeggio Sequence No. 1: 1-4-5, b3-5-b7, 4-b7-1, 5-1-b3, b7-b3-4, 1-4-5
+scalesequence.append({"Exercise":'Pentatonic Three Note Arpeggio Sequence No. 1', "Note Grouping":[3], "Sequence":[1,3,4,2,4,5,3,5,1,4,1,2,5,2,3,1,3,4]})
+
+# Major Pentatonic Three Note Arpeggio Sequence No. 2: 1-5-3, 6-1-5, 5-2-6, 6-3-1, 1-5-2, 2-6-3, 3-1-5 / # Minor Pentatonic Three Note Arpeggio Sequence No. 2: 1-5-4, b7-1-5, 5-b3-b7, b7-4-1, 1-5-b3, b3-b7-4, 4-1-5
+scalesequence.append({"Exercise":'Pentatonic Three Note Arpeggio Sequence No. 2', "Note Grouping":[3], "Sequence":[1,4,3,5,1,4,4,2,5,5,3,1,1,4,2,2,5,3,3,1,4]})
+
+# Major Pentatonic Four Note Sequence No. 1: 1-3-5-6, 1-6-5-3, 2-5-6-1, 2-1-6-5, 3-6-1-2, 3-2-1-6, 5-1-2-3, 5-3-2-1, 6-2-3-5, 6-5-3-2, 1-3-5-6 / # Minor Pentatonic Four Note Sequence No. 1: 1-4-5-b7, 1-b7-5-4, b3-5-b7-1, b3-1-b7-5, 4-b7-1-b3, 4-b3-1-b7, 5-1-b3-4, 5-4-b3-1, b7-b3-4-5, b7-5-4-b3, 1-4-5-b7
+scalesequence.append({"Exercise":'Pentatonic Four Note Sequence No. 1', "Note Grouping":[4], "Sequence":[1,3,4,5,1,5,4,3,2,4,5,1,2,1,5,4,3,5,1,2,3,2,1,5,4,1,2,3,4,3,2,1,5,2,3,4,5,4,3,2,1,3,4,5]})
+
+
 pattern.append({"Exercise":'Warm-up Exercise 1 Ascending', "H_Steps":['-H', 'W']})        # Hadrien Feraud Bass Warm-Up Exercise
 pattern.append({"Exercise":'Warm-up Exercise 1 Descending', "H_Steps":['H', '-W']})       # Hadrien Feraud Bass Warm-Up Exercise
 pattern.append({"Exercise":'Warm-up Exercise 2 Descending', "H_Steps":['-H', 'H', 'H']})  # Hadrien Feraud Bass Warm-Up Exercise
